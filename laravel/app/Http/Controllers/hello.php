4939187e7d719d8a97477492e9aa55964637fb0b
dpgaire <gairhedurga13@gmail.com>
2021-05-05 15:48:43 +0545

    condition using blade

--- laravel/app/Http/Controllers/hello.php
+++ laravel/app/Http/Controllers/hello.php
@@ -11,11 +11,14 @@ class Hello extends Controller
     //Send data from controller to view
     function index()
     {
+        $hari="Ram";
         $student=['Dp vai','Ram','Krishan','Hari','Shiva'];
+        $abc="No need to write <?php echo var ?>";
+        $script="<script>alert('This is Hacker From Nepal');</script>";
         //Multiple array 
         $marks=['20','21','22','23','24'];
         $address=['galyang','pokhara','syangja','chapakot','waling'];
-        return view('hello')->withstu($student)->withmks($marks)->withadd($address);
+        return view('hello')->withstu($student)->withmks($marks)->withadd($address)->withcba($abc)->withscript($script)->withhari($hari);
         // return view('hello')->with(['stu'=>$student],['mks'=>$marks]);
         // echo "Hello: ".$fname;
         // return view('hello');
--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -9,6 +9,35 @@
    <h1 class="text-center"><?php print_r($stu); ?> </h1>
    <h1 class="text-center"><?php print_r($mks); ?> </h1>
    <h1 class="text-center"><?php print_r($add); ?> </h1>
+   <h1 class="text-center">{{$cba}} </h1>
+   <!-- <h1 class="text-center">{{$script}} </h1> -->
+
+   <h1>
+    <!-- It will not run the script  -->
+    {{$script}}
+   </h1>
+   <h1>
+    <!-- The way of running the script  -->
+    {!! $script !!}
+   </h1>
+   <h1>
+    <!-- To tell laravel this not the part of laravel  -->
+    @{{$script}}
+   </h1>
+   <h1>{{$hari or "Not found"}}</h1>
+
+   <!-- If and else using blade syntax  -->
+   <?php $error="DATA NOT FOUND";?>
+   @if($hari=="Condition")
+   
+       {{ $hari }}
+   
+   @elseif($hari=="Ram")
+         {{ $hari }}
+   @else
+        {{ $error }}
+    @endif
+   
 @endsection
    
 
